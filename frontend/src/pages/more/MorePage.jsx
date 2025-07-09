c20615cd5489d4181dec31b9c1f0fa69ea8f503a
snyxe <153672410+Snyxex@users.noreply.github.com>
2025-07-09 08:03:19 +0200

    Update MorePage.jsx

--- frontend/src/pages/more/MorePage.jsx
+++ frontend/src/pages/more/MorePage.jsx
@@ -6,9 +6,8 @@ const MorePage = () => {
     <div className="landing-container">
       <h1 className="landing-title">Online Drive – Deine Cloud-Lösung</h1>
       <ul style={{ textAlign: 'left', maxWidth: '400px', margin: '2rem auto',color:'black' }}>
-        <li>Speichere und teile Dateien sicher in der Cloud</li>
+        <li>Speichere Dateien sicher in der Cloud</li>
         <li>Greife von überall auf deine Daten zu</li>
-        <li>Intuitive Oberfläche, auch für Teams und Gruppen</li>
         <li>Datenschutz und Sicherheit made in Germany</li>
         <li>Ideal für Schule, Studium und Alltag</li>
       </ul>
